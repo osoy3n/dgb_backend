4d23f125eb15513290a2a836d86c7666a8c2e843
Yohan D Osorio L <yohan.osorio@arroyoconsulting.net>
2025-02-05 22:23:17 -0500

    Update API title and welcome message

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI
 from database.db_config import init_db
 from routes import character_routes
 
-app = FastAPI(lifespan=init_db)
+app = FastAPI(lifespan=init_db, title="G-Padilla API")
 
 @app.get("/")
 async def root():
-    return {"mensaje": "Hola, bienvenido a D-Padilla API"}
+    return {"mensaje": "Hola, bienvenido a G-Padilla API"}
 
 app.include_router(character_routes.router, tags=["Personajes"])
