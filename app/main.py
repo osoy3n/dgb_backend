e0ae3e112d8f295746ad67157a0f65494bc7cb1d
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-02-21 17:40:48 -0500

    Add CORS middleware to enable cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database.db_config import init_db
 from routes import character_routes
 
 app = FastAPI(lifespan=init_db, title="G-Padilla API")
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
